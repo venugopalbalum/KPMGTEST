0cade2299d909c157003a5ed4f4b98c921276446
venugopalbalum <41577079+venugopalbalum@users.noreply.github.com>
2021-12-29 18:47:47 +0530

    RG Created

--- infrastructure/maint.tf
+++ infrastructure/maint.tf
@@ -1,1 +1,10 @@
-
+#Provider section
+provider   "azurerm"   { 
+   version   =   "= 2.0.0" 
+   features   {} 
+ }
+# Creater resource group
+resource   "azurerm_resource_group"   "rg"   { 
+   name   =   "my-first-terraform-rg" 
+   location   =   "eastus" 
+ }

7aa0599028b23fabf6fe9d36e2890b08f67ec9e5
venugopalbalum <41577079+venugopalbalum@users.noreply.github.com>
2021-12-29 19:24:02 +0530

    define subnet

--- infrastructure/maint.tf
+++ infrastructure/maint.tf
@@ -13,3 +13,17 @@ resource   "azurerm_resource_group"   "rg"   {
         environment = "Terraform Test"
     }
  }
+# Define Virtual Network and Subnet 
+resource   "azurerm_virtual_network"   "myvnet"   { 
+   name   =   "test-vnet" 
+   address_space   =   [ "10.0.0.0/16" ] 
+   location   =   "eastus" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+ } 
+#Subnet 
+ resource   "azurerm_subnet"   "frontendsubnet"   { 
+   name   =   "frontendSubnet" 
+   resource_group_name   =    azurerm_resource_group.rg.name 
+   virtual_network_name   =   azurerm_virtual_network.myvnet.name 
+   address_prefix   =   "10.0.1.0/24" 
+ }

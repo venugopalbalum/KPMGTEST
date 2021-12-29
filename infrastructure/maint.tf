95f652c42926e7305c29aedb892bf716cdfd88ba
venugopalbalum <41577079+venugopalbalum@users.noreply.github.com>
2021-12-29 19:49:19 +0530

    Updated VM

--- infrastructure/maint.tf
+++ infrastructure/maint.tf
@@ -27,3 +27,49 @@ resource   "azurerm_virtual_network"   "myvnet"   {
    virtual_network_name   =   azurerm_virtual_network.myvnet.name 
    address_prefix   =   "10.0.1.0/24" 
  }
+#Public IP
+resource   "azurerm_public_ip"   "myvm1publicip"   { 
+   name   =   "pip1" 
+   location   =   "eastus" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+   allocation_method   =   "Dynamic" 
+   sku   =   "Basic" 
+ }
+# NIC config
+resource   "azurerm_network_interface"   "myvm1nic"   { 
+   name   =   "myvm1-nic" 
+   location   =   "eastus" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+
+   ip_configuration   { 
+     name   =   "ipconfig1" 
+     subnet_id   =   azurerm_subnet.frontendsubnet.id 
+     private_ip_address_allocation   =   "Dynamic" 
+     public_ip_address_id   =   azurerm_public_ip.myvm1publicip.id 
+   } 
+ }
+# VM provision
+resource   "azurerm_windows_virtual_machine"   "example"   { 
+   name                    =   "testvm"   
+   location                =   "eastus" 
+   resource_group_name     =   azurerm_resource_group.rg.name 
+   network_interface_ids   =   [ azurerm_network_interface.myvm 1 nic.id ] 
+   size                    =   "Standard_B1s" 
+   admin_username          =   "XXXX" 
+   admin_password          =   "XXXXX" 
+
+   source_image_reference   { 
+     publisher   =   "MicrosoftWindowsServer" 
+     offer       =   "WindowsServer" 
+     sku         =   "2019-Datacenter" 
+     version     =   "latest" 
+   } 
+
+   os_disk   { 
+     caching             =   "ReadWrite" 
+     storage_account_type   =   "Standard_LRS" 
+   } 
+   tags = {
+        environment = "Terraform Test"
+    }
+ }
